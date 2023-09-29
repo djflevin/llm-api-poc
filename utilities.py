48e761ca55913ba6480318260c511f8a1099e156
Daniel Levin <80862052+djflevin@users.noreply.github.com>
2023-09-29 16:40:37 +0100

    Removed old file handlers functions

--- utilities.py
+++ utilities.py
@@ -1,47 +1,9 @@
-import json, yaml
 from dataclasses import dataclass
-from datetime import date
 from typing import Callable
 
 
 class DebugValues:
     verbose_logging = True
-
-
-def load_openapi_file(filename: str) -> str:
-    with open(filename, 'r') as f:
-        return f.read()
-
-@dataclass
-class WorkflowStep:
-    step_id: str
-    step_type: str
-    step_desc: str
-    openapi_doc: str
-    api_prompt: str
-    data_prompt: str
-
-def read_workflow_file(file_path):
-    """
-    Constructs a workflow from JSON file containing a series
-    of workflow steps.
-    """
-    with open(file_path, 'r') as file:
-        data = json.load(file)
-
-    workflow_steps = []
-    for step_data in data["workflow_steps"]:
-        new_step = WorkflowStep(
-            step_data["step_id"],
-            step_data["step_type"],
-            step_data["step_desc"],
-            load_openapi_file(step_data["openapi_file_location"]),
-            step_data["instruction"],
-            step_data["manipulation"]
-            )
-        workflow_steps.append(new_step)
-
-    return workflow_steps
 
 class PromptPreprocessor:
     def __init__(self, substitutions: dict[str: Callable]) -> None:
@@ -89,6 +51,7 @@ class PromptPreprocessor:
             index = s.find(pattern, index + len(pattern))
 
         return indexes
+
 
 if __name__ == "__main__":
     # Example usage:
