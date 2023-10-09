61a9ae75a5300b1e7b75725238d6b5b0652118d3
Daniel Levin <80862052+djflevin@users.noreply.github.com>
2023-10-09 15:28:08 +0100

    Callables

--- llmapi/callables.py
+++ llmapi/callables.py
@@ -3,4 +3,10 @@ from datetime import date
 def current_date():
     return date.today().isoformat()
 
-callables = {"TODAY" : current_date}
+def current_time():
+    return date.today().isoformat()
+
+def location_stub():
+    return "London, GB"
+
+callables = {"TODAY" : current_date, "TIME" : current_time, "LOCATION" : location_stub}
