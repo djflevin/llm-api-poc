ffed7f50041a9fb8a54d2b8ea75dd96101c1297c
Daniel Levin <80862052+djflevin@users.noreply.github.com>
2023-10-09 13:19:25 +0100

    Cleanup

--- llm-api/__init__.py
+++
@@ -1,6 +0,0 @@
-from dotenv import load_dotenv
-from os import getenv
-import openai
-
-load_dotenv()
-openai.api_key = getenv("OPENAI_API_KEY")
--- llm-api/callables.py
+++
@@ -1,6 +0,0 @@
-from datetime import date
-
-def current_date():
-    return date.today().isoformat()
-
-callables = {"TODAY" : current_date}
